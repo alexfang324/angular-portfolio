9ea2648cd90aaf14f8dfaa90edf4ab4987af9b67
alexfang324 <106351391+alexfang324@users.noreply.github.com>
2024-03-15 11:33:35 -0700

    finished researcher-section of the about me panel

--- src/app/components/aboutme/aboutme.component.ts
+++ src/app/components/aboutme/aboutme.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-aboutme',
@@ -10,11 +12,14 @@ import { Component } from '@angular/core';
 export class AboutmeComponent {
   observer?: IntersectionObserver;
 
+  constructor(private router: Router) {}
+
   ngOnInit(): void {
     this.wordByWordFadeIn();
   }
 
   ngAfterViewInit(): void {
+    this.wordByWordFadeIn();
     this.setupFadeInObserver();
   }
 
@@ -37,52 +42,48 @@ export class AboutmeComponent {
   }
 
   wordByWordFadeIn(): void {
-    document.addEventListener('DOMContentLoaded', function () {
-      var paragraphs = document.querySelectorAll('[class^="word-by-word"]');
+    var paragraphs = document.querySelectorAll('[class*="word-by-word"]');
+    paragraphs.forEach(function (paragraph) {
       var delay: number = 0;
+      var textNodes = paragraph.childNodes;
 
-      paragraphs.forEach(function (paragraph) {
-        var textNodes = paragraph.childNodes;
+      //find specified delay in classname if there is one
+      paragraph.classList.forEach((className) => {
+        // Check if the class starts with "word-by-word-"
+        if (className.startsWith('word-by-word-')) {
+          const matchResult = className.match(/\d+/)?.[0];
+          delay = matchResult ? parseInt(matchResult) : 0;
+        }
+      });
 
-        //find specified delay in classname if there is one
-        paragraph.classList.forEach((className) => {
-          // Check if the class starts with "word-by-word-"
-          if (className.startsWith('word-by-word-')) {
-            const matchResult = className.match(/\d+/)?.[0];
-            delay = matchResult ? parseInt(matchResult) : 0;
-            console.log('delay: ', delay);
-          }
-        });
+      // Replace text nodes with spans for each word
+      textNodes.forEach(function (node) {
+        if (node.nodeType === Node.TEXT_NODE) {
+          var words = (node as Text).data.trim().split(/\s+/);
+          var wordSpans = words.map((word: string | null) => {
+            var span = document.createElement('span');
+            span.textContent = word + ' ';
+            return span;
+          });
+          node.replaceWith(...wordSpans);
+        }
+      });
 
-        // Replace text nodes with spans for each word
-        textNodes.forEach(function (node) {
-          if (node.nodeType === Node.TEXT_NODE) {
-            var words = (node as Text).data.trim().split(/\s+/);
-            var wordSpans = words.map((word: string | null) => {
-              var span = document.createElement('span');
-              span.textContent = word + ' ';
-              return span;
-            });
-            node.replaceWith(...wordSpans);
-          }
-        });
+      // Remove empty spans, if any
+      paragraph.querySelectorAll('span').forEach((span) => {
+        if (!span.textContent!.trim()) {
+          span.remove();
+        }
+      });
 
-        // Remove empty spans, if any
-        paragraph.querySelectorAll('span').forEach((span) => {
-          if (!span.textContent!.trim()) {
-            span.remove();
-          }
-        });
-
-        // Hide and fade in each word
-        paragraph.querySelectorAll('span').forEach((span, index) => {
-          span.style.opacity = String(0);
-          setTimeout(function () {
-            span.style.transition = 'opacity 0.8s ease';
-            span.style.transitionDelay = delay + 's';
-            span.style.opacity = String(1);
-          }, 200 * index);
-        });
+      // Hide and fade in each word
+      paragraph.querySelectorAll('span').forEach((span, index) => {
+        span.style.opacity = String(0);
+        setTimeout(function () {
+          span.style.transition = 'opacity 0.8s ease';
+          span.style.transitionDelay = delay + 's';
+          span.style.opacity = String(1);
+        }, 200 * index);
       });
     });
   }
